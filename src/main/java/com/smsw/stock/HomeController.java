fd48c4f9ae38d315a9ef14ae2509c7544e0d509c
JMulungo <jj.mulungo@gmail.com>
2014-09-04 10:37:35 +0200

    :D

--- src/main/java/com/smsw/stock/HomeController.java
+++ src/main/java/com/smsw/stock/HomeController.java
@@ -19,9 +19,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
